15c9e8412d313c904395039d055d88f4521de828
Rody Davis <rody.davis.jr@gmail.com>
2020-06-22 20:05:11 -0700

    wip macos

--- macos/Classes/NativeDraggablePlugin.swift
+++ macos/Classes/NativeDraggablePlugin.swift
@@ -8,18 +8,67 @@ public class NativeDraggablePlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  var image: UIImage?
+  var previews: [String: UIView] = [:]
+  public func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+      if (image == nil){
+          return []
+      }
+      let provider = NSItemProvider(object: image!)
+      let item = UIDragItem(itemProvider: provider)
+      item.localObject = image
+      return [item]
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "setDraggableFeedback":
+     case "setDraggableFeedback":
+        let appDelegate  = UIApplication.shared.delegate as! FlutterAppDelegate
+        let viewController = appDelegate.window!.rootViewController as! FlutterViewController
         let args = call.arguments as! [String: Any]
+        let key = args["key"] as! String
+        print("setDraggableFeedback -> " + key)
+        print("Previews: \(previews.count)")
         let width = args["width"] as! Double
         let height = args["height"] as! Double
         let dx = args["dx"] as! Double
         let dy = args["dy"] as! Double
         let bytes = args["bytes"] as! [Int]
-        print("Size: [\(width)x\(height)]")
-        print("Offset: (\(dx),\(dy))")
-        print("Bytes: \(bytes)")
+        if (previews[key] != nil) {
+            previews[key]?.removeFromSuperview()
+        }
+        var array: [UInt8] = []
+        for item in bytes {
+            array.append(UInt8(item))
+        }
+        let datos: Data = Data(array)
+        image = UIImage(data: datos)!
+        let testFrame = CGRect(x: dx, y: dy, width: width, height: height)
+        let preview : UIView = UIView(frame: testFrame)
+        preview.backgroundColor = UIColor(patternImage: image!)
+        previews[key] = preview
+        let dragInteraction = UIDragInteraction(delegate: self)
+        preview.addInteraction(dragInteraction)
+        viewController.view.addSubview(preview)
+        result(true)
+    case "updateDraggableFeedback":
+        let args = call.arguments as! [String: Any]
+        let key = args["key"] as! String
+        let width = args["width"] as! Double
+        let height = args["height"] as! Double
+        let dx = args["dx"] as! Double
+        let dy = args["dy"] as! Double
+        let bytes = args["bytes"] as! [Int]
+        var array: [UInt8] = []
+        for item in bytes {
+            array.append(UInt8(item))
+        }
+        let datos: Data = Data(array)
+        image = UIImage(data: datos)!
+        if (previews[key] != nil) {
+            previews[key]?.frame =  CGRect(x: dx, y: dy, width: width, height: height)
+            previews[key]?.backgroundColor = UIColor(patternImage: image!)
+        }
         result(true)
     default:
       result(FlutterMethodNotImplemented)
